b9fd98e5c78b6b76e85910d461fbbc1b745ea921
dnsge <sagedanielr@gmail.com>
2018-10-14 19:11:24 -0400

    More documentation

--- src/org/dnsge/util/tableprinter/column/TableColumn.java
+++ src/org/dnsge/util/tableprinter/column/TableColumn.java
@@ -2,16 +2,35 @@ package org.dnsge.util.tableprinter.column;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Class that represents a Column in a table
+ *
+ * @param <T> Type of elements in the {@code TableColumn}
+ * @author Daniel Sage
+ * @version 1.2
+ */
 public class TableColumn<T> {
     private final String columnName;
     private final ArrayList<T> columnData;
 
+    /**
+     * Create an empty {@code TableColumn} with a name
+     *
+     * @param columnName Name of the column
+     */
     public TableColumn(String columnName) {
         this.columnName = columnName;
         this.columnData = new ArrayList<>();
     }
 
+    /**
+     * Create a {@code TableColumn} with an array of preexisting values
+     *
+     * @param columnName Name of the column
+     * @param values Values in the column
+     */
     public TableColumn(String columnName, T[] values) {
         this.columnName = columnName;
         this.columnData = new ArrayList<>(Arrays.asList(values));
@@ -49,14 +68,23 @@ public class TableColumn<T> {
         return longest.toString().length() > columnName.length() ? longest.toString().length() : columnName.length();
     }
 
+    /**
+     * @return name of the column
+     */
     public String getColumnName() {
         return columnName;
     }
 
-    public ArrayList<T> getColumnData() {
+    /**
+     * @return data inside the column
+     */
+    public List<T> getColumnData() {
         return columnData;
     }
 
+    /**
+     * @return number of items in the column
+     */
     public int length() {
         return columnData.size();
     }
