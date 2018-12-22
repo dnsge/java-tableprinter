0b4b649dd2cae078fbf0f57ce3840262a15f2f9a
dnsge <sagedanielr@gmail.com>
2018-12-22 17:54:41 -0500

    Increased test coverage

--- src/test/java/org/dnsge/util/tableprinter/TablePrinterTest.java
+++ src/test/java/org/dnsge/util/tableprinter/TablePrinterTest.java
@@ -4,11 +4,13 @@ import org.dnsge.util.tableprinter.column.TableColumn;
 import org.dnsge.util.tableprinter.row.TableItem;
 import org.dnsge.util.tableprinter.row.TableRow;
 import org.dnsge.util.tableprinter.row.TableRowItem;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TablePrinterTest {
 
@@ -53,6 +55,15 @@ class TablePrinterTest {
                 "1  | 234 | 5678910",
                 "56 | 2   | 1824.5 ",
         }, TablePrinter.rowsToStringTable(tableRow1, tableRow2));
+    }
+
+    @Test
+    @DisplayName("rowsToStringTable() fail")
+    void rowsToStringTableFail() {
+        TableRow tableRow1 = new TableRow(List.of("A", "b", "c"), List.of("1", "234", "5678910"));
+        TableRow tableRow2 = new TableRow(List.of("1", "2", "3"), List.of("56", "2", "1824.5"));
+
+        assertThrows(IllegalArgumentException.class, () -> TablePrinter.rowsToStringTable(tableRow1, tableRow2));
     }
 
     @Test
